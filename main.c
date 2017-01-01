1b7ce70498bd544e789a374d0c6ad15738d88929
janumeke <janumeke@gmail.com>
2017-01-01 22:06:35 +0800

    Update main.c
    Final

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+//build with gcc -lm -lrt -lpthread
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -7,15 +8,18 @@
 #include <pthread.h>
 //constant
 const int MAX_FLOOR=20;
-const float DEFAULT_RATE=0.004; //new users per millisecond
+const float DEFAULT_RATE=2; //new users per second
+const int ELEVATOR_SPEED=100000; //in microsecond
+const int ELEVATOR_WAIT_FOR_USER=0; //to leave or enter, in microsecond
 //semaphores
-sem_t elevatorCall;
+sem_t elevatorUsage;
 sem_t waitingListSem,passengerListSem;
 //shared variables
 typedef struct waitingNodePrototype{
     int atFloor;
     int direction;
     sem_t* elevatorIsHere;
+    struct waitingNodePrototype* previous;
     struct waitingNodePrototype* next;
 }waitingNode;
 waitingNode* waitingListHead=NULL;
@@ -24,18 +28,19 @@ typedef struct passengerNodePrototype{
     pthread_t tid;
     int toFloor;
     sem_t* elevatorIsHere;
+    struct passengerNodePrototype* previous;
     struct passengerNodePrototype* next;
 }passengerNode;
 passengerNode* passengerListHead=NULL;
 passengerNode* passengerListTail=NULL;
-//threads
+//thread attribute
 pthread_attr_t threadAttribute;
 
-inline float cumulativeProbability(float,float);
-void CreateElevatorThread();
-void CreateNewUserThread();
-void Elevator();
-void User();
+inline float nextTime(float rate);
+inline void CreateElevatorThread();
+inline void CreateNewUserThread();
+void* Elevator();
+void* User();
 
 int main(int argc,char* argv[]){
     float rate;
@@ -43,55 +48,66 @@ int main(int argc,char* argv[]){
         rate=DEFAULT_RATE;
     else if(argc>2){
         printf("Wrong Usage\n");
-        printf("Argument: ([rate])");
+        printf("Argument: [rate=%.2f]\n",DEFAULT_RATE);
+        printf("rate - number of users appearing every second\n");
         return 0;
     }
     else
         rate=atof(argv[1]);
     srand(time(NULL));
 
-    //initialize
-    sem_init(&elevatorCall,0,0);
+    //initialize semaphores and thread attribute
+    sem_init(&elevatorUsage,0,0);
     sem_init(&waitingListSem,0,1);
     sem_init(&passengerListSem,0,1);
     pthread_attr_init(&threadAttribute);
 
     CreateElevatorThread();
 
-    clock_t last=clock();
     while(1){
-        clock_t now=clock();
-        if(now-last<100) //millisecond=0.1s
-            usleep(35000); //microsecond=0.035s
-        else if( (float)rand()/RAND_MAX >= cumulativeProbability((float)(now-last),rate) ){
-            last=clock();
-            CreateNewUserThread();
+        float waitTime=nextTime(rate)*1000000;
+        if(waitTime/1000000>=1)
+            usleep((int)waitTime/1000000*1000000);
+        struct timespec last;
+        clock_gettime(CLOCK_REALTIME,&last);
+        waitTime-=(int)waitTime/1000000*1000000;
+        struct timespec now;
+        clock_gettime(CLOCK_REALTIME,&now);
+        long nanoInterval=(now.tv_nsec-last.tv_nsec)>=0?(now.tv_nsec-last.tv_nsec):(now.tv_nsec-last.tv_nsec+1000000000);
+        while(nanoInterval<waitTime){
+            usleep(waitTime/3);
+            clock_gettime(CLOCK_REALTIME,&now);
+            nanoInterval=(now.tv_nsec-last.tv_nsec)>=0?(now.tv_nsec-last.tv_nsec):(now.tv_nsec-last.tv_nsec+1000000000);
         }
-        else
-            usleep(1000); //microsecond=0.001s
+        CreateNewUserThread();
     }
     return 0;
 }
-inline float cumulativeProbability(float elapsedTime,float rate){
-    return 1-exp(-1*rate*elapsedTime);
+inline float nextTime(float rate){
+    return -logf(1.0-(float)rand()/(RAND_MAX+1))/rate;
 }
-void CreateElevatorThread(){
+inline void CreateElevatorThread(){
     pthread_t elevator;
-    pthread_create(&elevator,&threadAttribute,NULL,Elevator);
+    pthread_create(&elevator,&threadAttribute,Elevator,NULL);
     return;
 }
-void CreateNewUserThread(){
+inline void CreateNewUserThread(){
     pthread_t newUser;
-    pthread_create(&newUser,&threadAttribute,NULL,User);
+    pthread_create(&newUser,&threadAttribute,User,NULL);
     return;
 }
 
-void Elevator(){
+void* Elevator(){
     int atFloor=rand()%MAX_FLOOR+1,toFloor=atFloor;
+    printf("[電梯] %dF\n",atFloor);
     while(1){
         //wait for any user
-        if(waitingListHead==NULL)
-            sem_wait(&elevatorCall);
+        sem_wait(&waitingListSem); //critical section-waitingList
+            if(waitingListHead==NULL)
+                printf("(電梯閒置)\n");
+        sem_post(&waitingListSem);
+        sem_wait(&elevatorUsage);
+        sem_post(&elevatorUsage);
 
         //choose destination
         int lowestFloor=atFloor,highestFloor=atFloor;
@@ -132,6 +148,8 @@ void Elevator(){
                 }
             }
         }
+        if(toFloor!=atFloor)
+            printf("(電梯前往 %dF)\n",toFloor);
 
         //go to destination by 1 floor
         while(1){
@@ -140,17 +158,24 @@ void Elevator(){
             sem_wait(&passengerListSem); //critical section-passengerList
                 passengerNode* aPassengerNode=passengerListHead;
                 while(aPassengerNode!=NULL){
-                    passengerNode* nextPassengerNode=aPassengerNode->next;
                     if(aPassengerNode->toFloor==atFloor){
+                        pthread_t leavingPassengerThread=aPassengerNode->tid;
+                        passengerNode* previousPassengerNode=aPassengerNode->previous;
                         //signal user to leave
                         sem_post(aPassengerNode->elevatorIsHere);
+            sem_post(&passengerListSem);
                         //wait for user to successfully leave
-                        pthread_t leavingPassengerThread=aPassengerNode->tid;
-                        sem_post(&passengerListSem);
-                            pthread_join(leavingPassengerThread,NULL);
-                        sem_wait(&passengerListSem);
+                        while(pthread_join(leavingPassengerThread,NULL)!=0){
+                            usleep(ELEVATOR_WAIT_FOR_USER);
+                        }
+            sem_wait(&passengerListSem);
+                        if(previousPassengerNode==NULL)
+                            aPassengerNode=passengerListHead;
+                        else
+                            aPassengerNode=previousPassengerNode->next;
                     }
-                    aPassengerNode=nextPassengerNode;
+                    else
+                        aPassengerNode=aPassengerNode->next;
                 }
             sem_post(&passengerListSem);
             //let in users taking at this floor with same direction
@@ -158,46 +183,82 @@ void Elevator(){
             sem_wait(&waitingListSem); //critical section-waitingList
                 waitingNode* aWaitingNode=waitingListHead;
                 while(aWaitingNode!=NULL){
-                    waitingNode* nextWaitingNode=aWaitingNode->next;
                     if(aWaitingNode->atFloor==atFloor
-                       && (toFloor-atFloor)*aWaitingNode->direction>=0){
+                       && (toFloor-atFloor)*aWaitingNode->direction>=0){ //elevator has the same direction as user, or no direction
+                        sem_t* elevatorIsHere=aWaitingNode->elevatorIsHere;
+                        waitingNode* previousWaitingNode=aWaitingNode->previous;
                         //signal user to enter
                         sem_post(aWaitingNode->elevatorIsHere);
+            sem_post(&waitingListSem);
                         //wait for user to successfully enter
-                        sem_t* elevatorIsHere=aWaitingNode->elevatorIsHere;
-                        sem_post(&waitingListSem);
-                            while(sem_getvalue(elevatorIsHere,NULL)>=0)
-                                usleep(10000); //microsecond=0.01s
-                        sem_wait(&waitingListSem);
+                        sem_wait(&passengerListSem); //critical section-passengerList
+                            while(passengerListTail==NULL){
+                        sem_post(&passengerListSem);
+                                usleep(ELEVATOR_WAIT_FOR_USER);
+                        sem_wait(&passengerListSem);
+                            }
+                            while(passengerListTail->elevatorIsHere!=elevatorIsHere){
+                        sem_post(&passengerListSem);
+                                usleep(ELEVATOR_WAIT_FOR_USER);
+                        sem_wait(&passengerListSem);
+                            }
+                        sem_post(&passengerListSem);
 
                         //overwrite destination if farther
                         sem_wait(&passengerListSem); //critical section-passengerList
                             if(toFloor>atFloor){
-                                if(passengerListTail->toFloor>toFloor)
+                                if(passengerListTail->toFloor>toFloor){
                                     toFloor=passengerListTail->toFloor;
+                                    printf("(電梯改為前往 %dF)\n",toFloor);
+                                }
                             }
                             else if(toFloor<atFloor){
-                                if(passengerListTail->toFloor<toFloor)
+                                if(passengerListTail->toFloor<toFloor){
                                     toFloor=passengerListTail->toFloor;
+                                    printf("(電梯改為前往 %dF)\n",toFloor);
+                                }
                             }
-                            else
+                            else{
                                 toFloor=passengerListTail->toFloor;
+                                printf("(電梯前往 %dF)\n",toFloor);
+                            }
                         sem_post(&passengerListSem);
+            sem_wait(&waitingListSem);
+                        if(previousWaitingNode==NULL)
+                            aWaitingNode=waitingListHead;
+                        else
+                            aWaitingNode=previousWaitingNode->next;
                     }
-                    aWaitingNode=nextWaitingNode;
+                    else
+                        aWaitingNode=aWaitingNode->next;
                 }
             sem_post(&waitingListSem);
 
             if(toFloor==atFloor)
                 break;
+            struct timespec then;
+            clock_gettime(CLOCK_REALTIME,&then);
+            struct timespec now;
+            clock_gettime(CLOCK_REALTIME,&now);
+            long nanoInterval=(now.tv_nsec-then.tv_nsec)>=0?(now.tv_nsec-then.tv_nsec):(now.tv_nsec-then.tv_nsec+1000000000);
+            while(nanoInterval<ELEVATOR_SPEED*1000){
+                usleep(ELEVATOR_SPEED/3);
+                clock_gettime(CLOCK_REALTIME,&now);
+                nanoInterval=(now.tv_nsec-then.tv_nsec)>=0?(now.tv_nsec-then.tv_nsec):(now.tv_nsec-then.tv_nsec+1000000000);
+            }
             if(toFloor>atFloor)
                 ++atFloor;
             if(toFloor<atFloor)
                 --atFloor;
+            printf("[電梯] %dF\n",atFloor);
         }
     }
+    return;
 }
-void User(){
+void* User(){
+    static int nextNumber=1;
+    int userNumber=nextNumber++;
+
     //set random atFloor and toFloor
     int atFloor=rand()%MAX_FLOOR+1,toFloor=rand()%MAX_FLOOR+1;
     while(toFloor==atFloor)
@@ -212,6 +273,7 @@ void User(){
     sem_t elevatorIsHere;
     sem_init(&elevatorIsHere,0,0);
 
+    //stage 1 - wait for elevator
     //add to waitingList
     sem_wait(&waitingListSem); //critical section-waitingList
         waitingNode* previousWaitingNode=waitingListTail;
@@ -223,33 +285,32 @@ void User(){
             waitingListTail->next=malloc(sizeof(waitingNode));
             waitingListTail=waitingListTail->next;
         }
-        waitingListTail->atFloor=atFloor;
-        waitingListTail->direction=direction;
-        waitingListTail->elevatorIsHere=&elevatorIsHere;
-        waitingListTail->next=NULL;
+        waitingNode* thisWaitingUser=waitingListTail; //saved for removing
+        thisWaitingUser->atFloor=atFloor;
+        thisWaitingUser->direction=direction;
+        thisWaitingUser->elevatorIsHere=&elevatorIsHere;
+        thisWaitingUser->previous=previousWaitingNode;
+        thisWaitingUser->next=NULL;
+        printf("[使用者 %d] %dF,%s\n",userNumber,atFloor,direction>0?"↑":"↓");
     sem_post(&waitingListSem);
 
     //signal elevator to operate
-    if(sem_getvalue(&elevatorCall,NULL)<0)
-        sem_post(&elevatorCall);
+    sem_post(&elevatorUsage);
     //wait for elevator to arrive
     sem_wait(&elevatorIsHere);
 
+    //stage 2 - take the elevator
     //remove from waitingList
     sem_wait(&waitingListSem); //critical section-waitingList
-        waitingNode* nextWaitingNode;
-        if(previousWaitingNode==NULL){
-            nextWaitingNode=waitingListHead->next;
-            free(waitingListHead);
-            waitingListHead=nextWaitingNode;
-        }
-        else{
-            nextWaitingNode=previousWaitingNode->next->next;
-            free(previousWaitingNode->next);
-            previousWaitingNode->next=nextWaitingNode;
-        }
-        if(nextWaitingNode==NULL)
-            waitingListTail=previousWaitingNode;
+        if(thisWaitingUser->previous==NULL)
+            waitingListHead=thisWaitingUser->next;
+        else
+            thisWaitingUser->previous->next=thisWaitingUser->next;
+        if(thisWaitingUser->next==NULL)
+            waitingListTail=thisWaitingUser->previous;
+        else
+            thisWaitingUser->next->previous=thisWaitingUser->previous;
+        free(thisWaitingUser);
     sem_post(&waitingListSem);
     //add to passengerList
     sem_wait(&passengerListSem); //critical section-passengerList
@@ -262,32 +323,34 @@ void User(){
             passengerListTail->next=malloc(sizeof(passengerNode));
             passengerListTail=passengerListTail->next;
         }
-        passengerListTail->tid=pthread_self();
-        passengerListTail->toFloor=toFloor;
-        passengerListTail->elevatorIsHere=&elevatorIsHere;
-        passengerListTail->next=NULL;
+        passengerNode* thisPassenger=passengerListTail; //saved for removing
+        thisPassenger->tid=pthread_self();
+        thisPassenger->toFloor=toFloor;
+        thisPassenger->elevatorIsHere=&elevatorIsHere;
+        thisPassenger->previous=previousPassengerNode;
+        thisPassenger->next=NULL;
+        printf("(使用者 %d 進入電梯，按了 %dF 鍵)\n",userNumber,toFloor);
     sem_post(&passengerListSem);
 
     //wait for elevator to arrive
     sem_wait(&elevatorIsHere);
 
+    //stage 3 - leave the elevator
     //remove from passengerList
     sem_wait(&passengerListSem); //critical section-passengerList
-        passengerNode* nextPassengerNode;
-        if(previousPassengerNode==NULL){
-            nextPassengerNode=passengerListHead->next;
-            free(passengerListHead);
-            passengerListHead=nextPassengerNode;
-        }
-        else{
-            nextPassengerNode=previousPassengerNode->next->next;
-            free(previousPassengerNode->next);
-            previousPassengerNode->next=nextPassengerNode;
-        }
-        if(nextPassengerNode==NULL)
-            passengerListTail=previousPassengerNode;
+        if(thisPassenger->previous==NULL)
+            passengerListHead=thisPassenger->next;
+        else
+            thisPassenger->previous->next=thisPassenger->next;
+        if(thisPassenger->next==NULL)
+            passengerListTail=thisPassenger->previous;
+        else
+            thisPassenger->next->previous=thisPassenger->previous;
+        free(thisPassenger);
+        printf("(使用者 %d 離開電梯)\n",userNumber);
     sem_post(&passengerListSem);
 
     sem_destroy(&elevatorIsHere);
+    sem_wait(&elevatorUsage);
     return;
 }
